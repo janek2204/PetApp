9acae8c1f6aad570792e59e60ac1237aef47acd7
piotr <piotr.jankowski2204@gmail.com>
2022-01-19 11:48:28 +0000

    pet owner schema added

--- PetOwnerModel.js
+++ PetOwnerModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const petCarerSchema = new mongoose.Schema(
+const petOwnerSchema = new mongoose.Schema(
   {
     firstname: { type: String, required: true, maxlength: 15, minlength: 2 },
     lastname: { type: String, required: true, maxlength: 20, minlength: 5 },
@@ -9,9 +9,13 @@ const petCarerSchema = new mongoose.Schema(
     phonenumber: { type: Number, minlength: 8 },
     profileimage: { type: String, required: false, maxlength: 50 },
     aboutme: { type: String, maxlength: 500, required: true },
-    reviews: [{}], //here will go reviews from pet owners
-    preferedPets: { type: String, required: true },
-    petslookedafter: [{}], //here will go pets which carer was looking after
+    petdata: {
+      name: { type: String, required: true, maxlength: 15 },
+      image: { type: String, required: true },
+      age: { type: Number, maxlength: 2, required: true },
+      description: { type: String, required: true, maxlength: 500 },
+      reviews: [{}], // here will go reviews from carers
+    },
     address: {
       street: { type: String, required: true, minlength: 2 },
       housenumber: { type: Number, required: true, minlength: 1, maxlength: 5 },
