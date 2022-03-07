804a2720a7be6f6981f08d988bc74974782bb113
piotr <piotr.jankowski2204@gmail.com>
2022-03-07 12:06:56 +0000

    api request functions added to all neded components

---
+++ frontend/components/AllAdverts.js
@@ -0,0 +1,22 @@
+import Container from "react-bootstrap/Container";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
+const AllAdverts = () => {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get("/api/petCarers");
+      console.log(data);
+    };
+    getData();
+  }, []);
+
+  return()
+}
+
+export default AllAdverts
---
+++ frontend/components/Login.js
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
+const Login = () => {
+
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get("/api/petCarers");
+      console.log(data);
+    };
+    getData();
+  }, []);
+  
+  return()
+}
+
+export default Login
