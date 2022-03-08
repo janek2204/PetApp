b0accbcdf62b3b62e5ead1e8749067375959a6a5
piotr <piotr.jankowski2204@gmail.com>
2022-03-08 13:23:08 +0000

    footer fixed to the bottom

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import { Header, Menu } from "semantic-ui-react";
 
 const Footer = () => {
   return (
-    <Menu inverted color="olive" widths={1}>
+    <Menu inverted color="olive" widths={1} fixed="bottom">
       <Menu.Item>
         <Header textAlign="center" style={{ color: "white", fontSize: 20 }}>
           PetApp 2022
